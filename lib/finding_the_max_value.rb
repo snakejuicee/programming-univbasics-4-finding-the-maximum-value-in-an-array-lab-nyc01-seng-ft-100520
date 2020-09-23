03c831c43853ca3853ee9fccdee8d7ebaf85adc8
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-23 06:14:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   # Add your solution here
   
   x = 0
-  for i in 0..array.length()
+  for i in 0...array.length()
     puts array[i]
     puts x
     puts array[i].class
